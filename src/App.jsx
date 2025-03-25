d92ed991a8d4fe167129599f4437e7470c137e36
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-25 15:09:01 +0100

    add Header component and integrate it into App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
+import Header from './components/Header'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-
+      <Header />
     </>
   )
 }
