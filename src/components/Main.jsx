94d0511ca92bbf958ad24acfb0eaae6bce504ed2
vincenzo-terracciano <vincenzo.terracciano97@gmail.com>
2025-03-25 17:09:39 +0100

    refactor Main component to import languages data from a separate file

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,37 +1,5 @@
 import { useState } from "react"
-
-const languages = [
-    {
-        id: 1,
-        name: "HTML",
-        description: "HTML (HyperText Markup Language) is the standard language for creating web pages and applications. It structures web content and provides basic elements such as headings, paragraphs, and images."
-    },
-    {
-        id: 2,
-        name: "CSS",
-        description: "CSS (Cascading Style Sheets) is the language used to define the style of an HTML document, including colors, layout, and fonts."
-    },
-    {
-        id: 3,
-        name: "JavaScript",
-        description: "JavaScript is a programming language that allows you to make web pages dynamic."
-    },
-    {
-        id: 4,
-        name: "Node.js",
-        description: "Node.js is a server side JavaScript runtime that allows you to run JavaScript code outside the browser."
-    },
-    {
-        id: 5,
-        name: "Express",
-        description: "Express is a framework for Node.js that makes it easy to build web servers and API."
-    },
-    {
-        id: 6,
-        name: "ReactJS",
-        description: "ReactJS is a JavaScript library for building component-based user interfaces."
-    }
-];
+import languages from '../data/languages'
 
 export default function Main() {
     const [isOpen, setIsOpen] = useState(1)
